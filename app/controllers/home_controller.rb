dfebc757b2475de9ed33951aeb4c400226b9f4af
Vlad Otrocol <vladotrocol@yahoo.com>
2014-11-23 18:34:09 +0200

    admin panel working nav

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,26 @@ class HomeController < ApplicationController
   def index
   	@users = User.all
   end
+
+  def get_all_tables(myTables)
+    ActiveRecord::Base.connection.tables.each do |table|
+        next if table.match(/\Aschema_migrations\Z/)
+        next if table.match(/\Aauthentications\Z/)
+        klass = table.singularize.camelize.constantize      
+        myTables[klass.name] = klass
+    end
+  end
+
+  def admin
+    @DB = Hash.new
+    get_all_tables(@DB)
+  end
+
+  def admin_show
+    @user = User.find(params[:id])
+    @DB = Hash.new
+    get_all_tables(@DB)
+    @cols = @DB[params[:tb]].columns
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122094110) do
+ActiveRecord::Schema.define(version: 20141123140815) do
 
   create_table "authentications", force: true do |t|
     t.integer  "user_id"
     t.string   "provider"
     t.string   "uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "courses", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
